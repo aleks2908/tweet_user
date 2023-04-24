242f8d694850a008e99ec662170429043483d453
Oleksandr Druzhynin <aleks_str78@ukr.net>
2023-04-24 09:36:01 +0300

    homepage

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,23 @@
 import "modern-normalize";
 import { Routes, Route } from "react-router-dom";
-// import { Toaster } from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 import Home from "../pages/Home/Home";
 import Tweets from "../pages/Tweets/Tweets";
 
+
+// const Home = lazy(() => import("pages/Home"));
+// const Tweets = lazy(() => import("pages/Tweets"));
+
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/tweets" element={<Tweets />} />
-      <Route path="*" element={<Home />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tweets" element={<Tweets />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+      <Toaster position="top-right" reverseOrder={false} />
+    </>
   );
 }
 
-// export default function App() {
-//   return (
-//     <>
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/tweets" element={<Tweets />} />
-//         <Route path="*" element={<Home />} />
-//       </Routes>
-//       <Toaster position="top-right" reverseOrder={false} />
-//     </>
-//   );
-// }
